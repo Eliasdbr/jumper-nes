4333a243af8b68594169da29fc78e639a489a240
eliasdbr <eliasdbr@outlook.com>
2025-12-17 00:04:50 -0300

    Added metatile library

---
+++ lib/metatile.asm
@@ -0,0 +1,24 @@
+;
+;	Librería de Metatiles
+;	por Eliasdbr (eliasdbr@outlook.com)
+;	Para la NES/Famicom 
+;	***Escrito para ASM6 v1.6***
+
+;;	DEFINICIONES
+
+
+;;	VARIABLES DE PÁGINA CERO
+
+
+;;	OTRAS VARIABLES
+
+
+;;	MACROS
+
+
+;; SUB-RUTINAS
+.base ASM_PROGRAM
+
+
+
+ASM_PROGRAM = $
